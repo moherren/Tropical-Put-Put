c7b55d858978be740644eac5499a2e8e6f74a09c
Matthew "The Hammer" O'Herren <moherren@gmail.com>
2016-03-31 13:50:42 -0400

    Work on text boxes

---
+++ src/game/TextBox.java
@@ -0,0 +1,16 @@
+package game;
+
+import graphics.Render;
+
+public class TextBox {
+	public final static int TB_HELP=0,TB_CHAT=1,TB_INSTRUCT=2;
+	int type;
+	int x,y;
+	Render box;
+	
+	
+	private TextBox(int type,int x,int y,int width,int height,String text){
+		
+	}
+	
+}
