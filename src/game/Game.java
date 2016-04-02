4278435fa32135fb2b2aa0a71dfd628530638a2e
Peter <Peter@192.168.2.8>
2016-04-02 10:15:19 -0400

    resolving conflicts

--- src/game/Game.java
+++ src/game/Game.java
@@ -17,7 +17,6 @@ import javax.swing.Timer;
 
 import course.GolfCourse;
 import course.Grass;
-import course.Ice;
 import course.Wall;
 import entities.GolfBall;
 import geometry.Rectangle;
@@ -36,7 +35,7 @@ public class Game implements VisibleObject,KeyListener, MouseListener, Runnable,
 	public static final int SC_MAIN_MENU=0,SC_GOLF_GAME=1,SC_CUT_SCENE=2,SC_TUTORIAL_GAME=3,SC_SETTINGS_GAME=4;
 	public Display display;
 	boolean running=true;
-	GolfCourse course,c1,c2;
+	GolfCourse c;
 	int screen=SC_MAIN_MENU;
 	Background background;
 	GUI gui;
@@ -63,18 +62,13 @@ public class Game implements VisibleObject,KeyListener, MouseListener, Runnable,
 		menuButtons=MenuButton.getMenuButtons(this);
 		settingsButtons=MenuButton.getSettingsButtons(this);
 		
-		c1=new GolfCourse(new Vector2D(400,400), 5);
-		c1.addObstacle(new Wall(new Rectangle(40,250,40,300)));
-		c1.addObstacle(new Wall(new Rectangle(400,80,760,40)));
-		c1.addObstacle(new Wall(new Rectangle(400,420,760,40)));
-		c1.addObstacle(new Wall(new Rectangle(760,250,40,300)));
-		c1.addSurface(new Grass(new Rectangle(500,250,400,300)));
-		c2=new GolfCourse(new Vector2D(400,400), 5);
-		c2.addObstacle(new Wall(new Rectangle(40,250,40,300)));
-		c2.addObstacle(new Wall(new Rectangle(400,80,760,40)));
-		c2.addObstacle(new Wall(new Rectangle(400,420,760,40)));
-		c2.addObstacle(new Wall(new Rectangle(760,250,40,300)));
-		c2.addSurface(new Ice(new Rectangle(500,250,400,300)));
+		c=new GolfCourse();
+		c.addObstacle(new Wall(new Rectangle(40,250,40,300)));
+		c.addObstacle(new Wall(new Rectangle(400,80,760,40)));
+		c.addObstacle(new Wall(new Rectangle(400,420,760,40)));
+		c.addObstacle(new Wall(new Rectangle(760,250,40,300)));
+		c.addSurface(new Grass(new Rectangle(500,250,400,300)));
+		gui=new GUI(c);
 		background=new Background(this);
 		SoundHandler.playMusic(SoundHandler.SONG_ONE, 0);
 		SoundHandler.setMusicVolume(volume);
@@ -104,7 +98,7 @@ public class Game implements VisibleObject,KeyListener, MouseListener, Runnable,
 		}
 		
 		case SC_GOLF_GAME:{
-			course.render(r);
+			c.render(r);
 			
 			if(putting){
 				double x=ball.getPosition().x;
@@ -249,7 +243,8 @@ public class Game implements VisibleObject,KeyListener, MouseListener, Runnable,
 		if(!playing){
 			new Timer((int)(1000/updatePerSecond),this).start();
 			playing=true;
-			loadCourse(c1);
+			ball=new GolfBall(new Vector2D(450,200),c);
+			c.addEntity(ball);
 			putting=true;
 			new Thread(){
 				public void run(){
@@ -259,8 +254,8 @@ public class Game implements VisibleObject,KeyListener, MouseListener, Runnable,
 						Vector2D newTilt=new Vector2D(scan.nextDouble(),scan.nextDouble()).normalize();
 						System.out.println("new angle:");
 						double newAngle=scan.nextDouble();
-						course.tiltDirection=newTilt;
-						course.tiltAngle=newAngle;
+						c.tiltDirection=newTilt;
+						c.tiltAngle=newAngle;
 					}
 				}
 			}.start();
@@ -268,24 +263,13 @@ public class Game implements VisibleObject,KeyListener, MouseListener, Runnable,
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		course.update((int)(1000/updatePerSecond)/10);
-		if(course.scored(ball)){
-			loadCourse(c2);
-		}
+		c.update((int)(1000/updatePerSecond)/10);
 		if(ball.getVelocity().isZeroed()&&!putting){
 			putting=true;
 		}
 		else if(putting&&mouseDown){
 			gui.powerLevel=gui.powerLevel%1+0.005;
 		}
-	}
-	
-	public void loadCourse(GolfCourse gc){
-		course=gc;
-		ball=new GolfBall(gc.ballStart,gc);
-		course.addEntity(ball);
-		gui=new GUI(course);
-		gui.parNum=course.par;
 	}
 
 	public void setVolume(double amount) {
