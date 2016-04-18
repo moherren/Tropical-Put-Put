9e377ab48eefb337ca297e579602946bc3942357
peter minch <peterminch575@gmail.com>
2016-04-18 13:36:00 -0400

    Added a center to Windmill so GolfBalls cannot get stuck in it. No more launching... :(

--- src/course/Windmill.java
+++ src/course/Windmill.java
@@ -7,6 +7,7 @@ import entities.Entity;
 import entities.GameEntity;
 import graphics.Render2D;
 import visibleObjects.VisibleObject;
+import geometry.Circle;
 import geometry.Polygon;
 import geometry.Rectangle;
 import geometry.Shape;
@@ -16,7 +17,8 @@ public class Windmill implements VisibleObject,Obstacle,Entity{
 
 //	private ArrayList<Polygon> fins;
 	private ArrayList<Wall> fins;
-	private double turnSpeed=1;//degrees
+	private Circle middle;
+	private double turnSpeed=0.5;//degrees
 	private double rotation=0;//degrees
 	private double elasticity=0.3;
 	private Vector2D center;
@@ -28,6 +30,7 @@ public class Windmill implements VisibleObject,Obstacle,Entity{
 		fins.add(new Wall(new Rectangle(center.x+finWidth,center.y-finWidth/2,finLength,finWidth)));
 		fins.add(new Wall(new Rectangle(center.x-finWidth/2,center.y-finWidth-finLength,finWidth,finLength)));
 		fins.add(new Wall(new Rectangle(center.x-finWidth/2,center.y+finWidth,finWidth,finLength)));
+		middle=new Circle(center.x,center.y,finWidth);
 	}
 	
 	@Override
@@ -36,6 +39,8 @@ public class Windmill implements VisibleObject,Obstacle,Entity{
 			if(w.intersects(entity))
 				return true;
 		}
+		if(middle.intersects(entity.shape))
+			return true;
 		return false;
 	}
 
@@ -50,6 +55,14 @@ public class Windmill implements VisibleObject,Obstacle,Entity{
 				entity.applyImpulse(impulse);
 			}
 		}
+		if(middle.intersects(entity.shape)){
+			Vector2D normal=entity.getPosition().sub(center);
+			Vector2D toEntity=center.sub(entity.getPosition());
+			Vector2D relativeVelocity=entity.getVelocity().add(toEntity.perpendicular().mult(Math.toRadians(turnSpeed)));
+			Vector2D impulse=normal.normalize().mult(Math.abs(relativeVelocity.dot(normal.negative()))/normal.magnitude()*(1+elasticity));
+			entity.applyImpulse(impulse);
+		}
+		
 	}
 
 	@Override
@@ -57,6 +70,7 @@ public class Windmill implements VisibleObject,Obstacle,Entity{
 		for(Wall w:fins){
 			w.render(g);
 		}
+		
 	}
 
 	@Override
@@ -64,6 +78,22 @@ public class Windmill implements VisibleObject,Obstacle,Entity{
 		for(Wall w:fins){
 			w.render(r);
 		}
+		int X=(int) center.x,Y=(int) center.y;
+		
+		int length=(int) middle.getRadius();
+		double perimeter=(length)*Math.PI*2;
+			for(double angle=Math.PI/2.0;angle<=Math.PI*3/2.0;angle+=Math.PI*2/perimeter){
+				
+				int x=(int) (Math.cos(angle)*length+X);
+				int y=(int) (Math.sin(angle)*length+Y);
+				int lineLength=(int) (Math.cos(angle)*2*length);
+				r.drawPixel(1,x,y);
+				for(int line=-1;line>lineLength;line--)
+					if(r.pixels[x+y*r.width-line]!=1)
+						r.drawPixel(0xff0000, x-line,y);
+				r.drawPixel(1,x-lineLength,y);
+				
+			}
 	}
 	
 	public void update(double t){
